fb6b6c78470abd6c77054f4521e32b157e852b73
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-11-30 15:19:48 +0100

    Création dossier et fichier de base

---
+++ calculatrice.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Nov 30 2023
+
+@author: Mazard Pierre
+
+#Calculatrice
+"""
+
+#Importation de fonctions externes (librairies) :
+    
+    
+#Définition locale de fonctions : 
+  
+    
+#Déclaration des variables : 
+
+
+#Corps principal du programme : 
+
